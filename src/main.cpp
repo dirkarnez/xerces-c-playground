76956f0ec7671e5882924ded3f824572bac0f1d1
Dirk Arnez <smalldirkalex@gmail.com>
2022-09-10 02:53:38 +0800

    - update src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <xercesc/util/XMLString.hpp>
 #include <xercesc/dom/DOM.hpp>
 #include <xercesc/util/OutOfMemoryException.hpp>
-
+#include <xercesc/framework/StdOutFormatTarget.hpp>
 #include <iostream>
 
 using namespace XERCES_CPP_NAMESPACE;
@@ -13,11 +13,11 @@ using namespace XERCES_CPP_NAMESPACE;
 // ---------------------------------------------------------------------------
 class XStr
 {
-public :
+public:
     // -----------------------------------------------------------------------
     //  Constructors and Destructor
     // -----------------------------------------------------------------------
-    XStr(const char* const toTranscode)
+    XStr(const char *const toTranscode)
     {
         // Call the private transcoding method
         fUnicodeForm = XMLString::transcode(toTranscode);
@@ -28,33 +28,31 @@ public :
         XMLString::release(&fUnicodeForm);
     }
 
-
     // -----------------------------------------------------------------------
     //  Getter methods
     // -----------------------------------------------------------------------
-    const XMLCh* unicodeForm() const
+    const XMLCh *unicodeForm() const
     {
         return fUnicodeForm;
     }
 
-private :
+private:
     // -----------------------------------------------------------------------
     //  Private data members
     //
     //  fUnicodeForm
     //      This is the Unicode XMLCh format of the string.
     // -----------------------------------------------------------------------
-    XMLCh*   fUnicodeForm;
+    XMLCh *fUnicodeForm;
 };
 
 #define X(str) XStr(str).unicodeForm()
-
 
 // ---------------------------------------------------------------------------
 //  main
 // ---------------------------------------------------------------------------
 
-int main(int argC, char*[])
+int main(int argC, char *[])
 {
     // Initialize the XML4C2 system.
     try
@@ -62,12 +60,12 @@ int main(int argC, char*[])
         XMLPlatformUtils::Initialize();
     }
 
-    catch(const XMLException& toCatch)
+    catch (const XMLException &toCatch)
     {
         char *pMsg = XMLString::transcode(toCatch.getMessage());
         std::cerr << "Error during Xerces-c Initialization.\n"
-             << "  Exception message:"
-             << pMsg;
+                  << "  Exception message:"
+                  << pMsg;
         XMLString::release(&pMsg);
         return 1;
     }
@@ -77,94 +75,105 @@ int main(int argC, char*[])
     if (argC > 1)
     {
         std::cout << "\nUsage:\n"
-                "    CreateDOMDocument\n\n"
-                "This program creates a new DOM document from scratch in memory.\n"
-                "It then prints the count of elements in the tree.\n"
-             << std::endl;
+                     "    CreateDOMDocument\n\n"
+                     "This program creates a new DOM document from scratch in memory.\n"
+                     "It then prints the count of elements in the tree.\n"
+                  << std::endl;
         errorCode = 1;
     }
-    if(errorCode) {
+    if (errorCode)
+    {
         XMLPlatformUtils::Terminate();
         return errorCode;
     }
 
-   {
-       //  Nest entire test in an inner block.
-       //  The tree we create below is the same that the XercesDOMParser would
-       //  have created, except that no whitespace text nodes would be created.
+    {
+        //  Nest entire test in an inner block.
+        //  The tree we create below is the same that the XercesDOMParser would
+        //  have created, except that no whitespace text nodes would be created.
 
-       // <company>
-       //     <product>Xerces-C</product>
-       //     <category idea='great'>XML Parsing Tools</category>
-       //     <developedBy>Apache Software Foundation</developedBy>
-       // </company>
+        // <company>
+        //     <product>Xerces-C</product>
+        //     <category idea='great'>XML Parsing Tools</category>
+        //     <developedBy>Apache Software Foundation</developedBy>
+        // </company>
 
-       DOMImplementation* impl =  DOMImplementationRegistry::getDOMImplementation(X("Core"));
+        DOMImplementation *impl = DOMImplementationRegistry::getDOMImplementation(X("Core"));
 
-       if (impl != NULL)
-       {
-           try
-           {
-               DOMDocument* doc = impl->createDocument(
-                           0,                    // root element namespace URI.
-                           X("company"),         // root element name
-                           0);                   // document type object (DTD).
+        if (impl != NULL)
+        {
+            try
+            {
+                DOMDocument *doc = impl->createDocument(
+                    0,            // root element namespace URI.
+                    X("company"), // root element name
+                    0);           // document type object (DTD).
 
-               DOMElement* rootElem = doc->getDocumentElement();
+                DOMElement *rootElem = doc->getDocumentElement();
 
-               DOMElement*  prodElem = doc->createElement(X("product"));
-               rootElem->appendChild(prodElem);
+                DOMElement *prodElem = doc->createElement(X("product"));
+                rootElem->appendChild(prodElem);
 
-               DOMText*    prodDataVal = doc->createTextNode(X("Xerces-C"));
-               prodElem->appendChild(prodDataVal);
+                DOMText *prodDataVal = doc->createTextNode(X("Xerces-C"));
+                prodElem->appendChild(prodDataVal);
 
-               DOMElement*  catElem = doc->createElement(X("category"));
-               rootElem->appendChild(catElem);
+                DOMElement *catElem = doc->createElement(X("category"));
+                rootElem->appendChild(catElem);
 
-               catElem->setAttribute(X("idea"), X("great"));
+                catElem->setAttribute(X("idea"), X("great"));
 
-               DOMText*    catDataVal = doc->createTextNode(X("XML Parsing Tools"));
-               catElem->appendChild(catDataVal);
+                DOMText *catDataVal = doc->createTextNode(X("XML Parsing Tools"));
+                catElem->appendChild(catDataVal);
 
-               DOMElement*  devByElem = doc->createElement(X("developedBy"));
-               rootElem->appendChild(devByElem);
+                DOMElement *devByElem = doc->createElement(X("developedBy"));
+                rootElem->appendChild(devByElem);
 
-               DOMText*    devByDataVal = doc->createTextNode(X("Apache Software Foundation"));
-               devByElem->appendChild(devByDataVal);
+                DOMText *devByDataVal = doc->createTextNode(X("Apache Software Foundation"));
+                devByElem->appendChild(devByDataVal);
 
-               //
-               // Now count the number of elements in the above DOM tree.
-               //
+                //
+                // Now count the number of elements in the above DOM tree.
+                //
 
-               const XMLSize_t elementCount = doc->getElementsByTagName(X("*"))->getLength();
-               std::cout << "The tree just created contains: " << elementCount
-                    << " elements." << std::endl;
+                const XMLSize_t elementCount = doc->getElementsByTagName(X("*"))->getLength();
+                std::cout << "The tree just created contains: " << elementCount << " elements." << std::endl;
 
-               doc->release();
-           }
-           catch (const OutOfMemoryException&)
-           {
-               std::cerr << "OutOfMemoryException" << std::endl;
-               errorCode = 5;
-           }
-           catch (const DOMException& e)
-           {
-               std::cerr << "DOMException code is:  " << e.code << std::endl;
-               errorCode = 2;
-           }
-           catch (...)
-           {
-               std::cerr << "An error occurred creating the document" << std::endl;
-               errorCode = 3;
-           }
-       }  // (inpl != NULL)
-       else
-       {
-           std::cerr << "Requested implementation is not supported" << std::endl;
-           errorCode = 4;
-       }
-   }
+                static const XMLCh gLS[] = {chLatin_L, chLatin_S, chNull};
+                DOMImplementationLS *impl = (DOMImplementationLS *)DOMImplementationRegistry::getDOMImplementation(gLS);
+                DOMLSSerializer *writer = DOMImplementation::getImplementation()->createLSSerializer();
+                DOMLSOutput *theOutputDesc = ((DOMImplementationLS *)impl)->createLSOutput();
+                XMLCh *gOutputEncoding = 0;
+                theOutputDesc->setEncoding(gOutputEncoding);
+                XMLFormatTarget *myFormTarget = new StdOutFormatTarget();
 
-   XMLPlatformUtils::Terminate();
-   return errorCode;
+                theOutputDesc->setByteStream(myFormTarget);
+                writer->write(doc, theOutputDesc);
+                writer->release();
+                doc->release();
+            }
+            catch (const OutOfMemoryException &)
+            {
+                std::cerr << "OutOfMemoryException" << std::endl;
+                errorCode = 5;
+            }
+            catch (const DOMException &e)
+            {
+                std::cerr << "DOMException code is:  " << e.code << std::endl;
+                errorCode = 2;
+            }
+            catch (...)
+            {
+                std::cerr << "An error occurred creating the document" << std::endl;
+                errorCode = 3;
+            }
+        } // (inpl != NULL)
+        else
+        {
+            std::cerr << "Requested implementation is not supported" << std::endl;
+            errorCode = 4;
+        }
+    }
+
+    XMLPlatformUtils::Terminate();
+    return errorCode;
 }
